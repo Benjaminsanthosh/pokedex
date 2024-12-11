da1370ecc5a555ad68af277cf6aa95c4bf9adafc
Benjamin Santhosh <benja.santhosh@gmail.com>
2024-12-11 12:24:05 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pokedex/presenter/app.dart';
 import 'package:pokedex/di.dart';
+import 'package:pokedex/presenter/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
